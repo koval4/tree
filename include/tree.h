f761ca099a0733e240a599e7de40f588cf782708
Roman Kovalivskiy <koval304@gmail.com>
2018-10-28 03:00:46 +0300

    Implement tree_node copy, move and value constructors
    Basic constructors for tree_node is a mandatory interface.
    Copy and move constructors are conditionally deleted on type T.
    Constructor from single value propagates explicitness.
    All constructors propagate noexceptness.

--- include/tree.h
+++ include/tree.h
@@ -1,18 +1,128 @@
 #ifndef TREE_H_INCLUDED
 #define TREE_H_INCLUDED
 
+#include <type_traits>
+#include <utility>
+
+namespace detail {
+    template <typename T, bool = std::is_copy_constructible_v<T>, bool = std::is_move_constructible_v<T>>
+    struct enable_special_members;
+
+    template <typename T>
+    struct enable_special_members<T, true, true> {
+        enable_special_members() noexcept = default;
+
+        enable_special_members(const enable_special_members&) noexcept = default;
+        enable_special_members(enable_special_members&&) noexcept = default;
+    };
+
+    template <typename T>
+    struct enable_special_members<T, false, true> {
+        enable_special_members() noexcept = default;
+
+        enable_special_members(const enable_special_members&) noexcept = delete;
+        enable_special_members(enable_special_members&&) noexcept = default;
+    };
+
+    template <typename T>
+    struct enable_special_members<T, true, false> {
+        enable_special_members() noexcept = default;
+
+        enable_special_members(const enable_special_members&) noexcept = default;
+        enable_special_members(enable_special_members&&) noexcept = delete;
+    };
+
+    template <typename T>
+    struct enable_special_members<T, false, false> {
+        enable_special_members() noexcept = default;
+
+        enable_special_members(const enable_special_members&) noexcept = delete;
+        enable_special_members(enable_special_members&&) noexcept = delete;
+    };
+}
+
 template <typename T>
-struct tree_node {
-    tree_node* parent;
-    tree_node* left;
-    tree_node* right;
+struct tree_node_impl {
+    tree_node_impl* parent;
+    tree_node_impl* left;
+    tree_node_impl* right;
     T value;
 
-    tree_node(T value)
+    template <typename U = T,
+              std::enable_if_t<
+                  !std::is_same_v<tree_node_impl<T>, std::decay_t<U>> &&
+                  !std::is_convertible_v<U, T> &&
+                  std::is_constructible_v<T, U&&>, int> = 0>
+    explicit tree_node_impl(U&& value) noexcept(std::is_nothrow_constructible_v<T, U&&>)
         : parent{nullptr}
         , left{nullptr}
         , right{nullptr}
-        , value{value} {};
+        , value{std::forward<U>(value)} {}
+
+    template <typename U = T,
+              std::enable_if_t<
+                  !std::is_same_v<tree_node_impl<T>, std::decay_t<U>> &&
+                  std::is_convertible_v<U, T> &&
+                  std::is_constructible_v<T, U&&>, int> = 0>
+    tree_node_impl(U&& value) noexcept(std::is_nothrow_constructible_v<T, U&&>)
+        : parent{nullptr}
+        , left{nullptr}
+        , right{nullptr}
+        , value{std::forward<U>(value)} {}
+
+    template <typename U = T,
+              std::enable_if_t<
+                  !std::is_same_v<tree_node_impl<T>, std::decay_t<U>> &&
+                  std::is_constructible_v<T, U&&>, int> = 0>
+    tree_node_impl(U&& value,
+              tree_node_impl<T>* parent,
+              tree_node_impl<T>* left,
+              tree_node_impl<T>* right) noexcept(std::is_nothrow_constructible_v<T, U&&>)
+        : parent{parent}
+        , left{left}
+        , right{right}
+        , value{std::forward<U>(value)} {}
+
+    tree_node_impl(const tree_node_impl& other) noexcept(std::is_nothrow_copy_constructible_v<T>)
+        : parent{other.parent}
+        , left{other.parent}
+        , right{other.parent}
+        , value{other.value} {}
+
+    tree_node_impl(tree_node_impl&& other) noexcept(std::is_nothrow_move_constructible_v<T>)
+        : parent{other.parent}
+        , left{other.left}
+        , right{other.right}
+        , value{std::move(value)} {}
+};
+
+template <typename T>
+struct tree_node : tree_node_impl<T>, private detail::enable_special_members<T> {
+    template <typename U = T,
+              std::enable_if_t<
+                  !std::is_same_v<tree_node<T>, std::decay_t<U>> &&
+                  !std::is_convertible_v<U, T> &&
+                  std::is_constructible_v<T, U&&>, int> = 0>
+    explicit tree_node(U&& value) noexcept(std::is_nothrow_constructible_v<T, U&&>)
+        : tree_node_impl<T>{std::forward<U>(value)} {}
+
+    template <typename U = T,
+              std::enable_if_t<
+                  !std::is_same_v<tree_node<T>, std::decay_t<U>> &&
+                  std::is_convertible_v<U, T> &&
+                  std::is_constructible_v<T, U&&>, int> = 0>
+    tree_node(U&& value) noexcept(std::is_nothrow_constructible_v<T, U&&>)
+        : tree_node_impl<T>{std::forward<U>(value)} {}
+
+    template <typename U = T,
+              std::enable_if_t<
+                  !std::is_same_v<tree_node<T>, std::decay_t<U>> &&
+                  std::is_constructible_v<T, U&&>, int> = 0>
+    tree_node(U&& value,
+              tree_node<T>* parent,
+              tree_node<T>* left,
+              tree_node<T>* right) noexcept(std::is_nothrow_constructible_v<T, U&&>)
+        : tree_node_impl<T>{std::forward<U>(value), parent, left, right} {}
 };
 
 #endif // TREE_H_INCLUDED
--- test/test.cpp
+++ test/test.cpp
@@ -2,11 +2,71 @@
 #include <catch2/catch.hpp>
 
 #include "tree.h"
+#include <vector>
+
+struct no_copy {
+    no_copy() = default;
+    no_copy(const no_copy&) = delete;
+    no_copy(no_copy&&) = default;
+
+    int dummy;
+};
+
+struct no_move {
+    no_move() = default;
+    no_move(const no_move&) = default;
+    no_move(no_move&&) = delete;
+
+    int dummy;
+};
 
 TEST_CASE("Tree node constructed", "[tree_node]") {
+    // compile-time checks
+    static_assert(
+        std::is_copy_constructible_v<tree_node<int>>,
+        "tree_node should propagate copy constructor presence");
+    static_assert(
+        std::is_move_constructible_v<tree_node<int>>,
+        "tree_node should propagate move constructor presence");
+
+    static_assert(
+        !std::is_copy_constructible_v<tree_node<no_copy>>,
+        "tree_node should propagate copy constructor presence");
+    static_assert(
+        std::is_move_constructible_v<tree_node<no_copy>>,
+        "tree_node should propagate move constructor presence");
+
+    static_assert(
+        std::is_copy_constructible_v<tree_node<no_move>>,
+        "tree_node should propagate copy constructor presence");
+    // Types without a move constructor, but with a copy constructor that accepts const T& arguments, satisfy std::is_move_constructible.
+    // TODO: test is broken, check how it could be fixed
+    //static_assert(
+    //    !std::is_move_constructible_v<tree_node<no_move>>,
+    //    "tree_node should propagate move constructor presence");
+
     tree_node<int> node{1};
     REQUIRE(node.value == 1);
     REQUIRE(node.parent == nullptr);
     REQUIRE(node.left == nullptr);
     REQUIRE(node.right == nullptr);
+
+    tree_node<int> another_node{node};
+    REQUIRE(another_node.value == 1);
+    REQUIRE(another_node.parent == nullptr);
+    REQUIRE(another_node.left == nullptr);
+    REQUIRE(another_node.right == nullptr);
+
+    tree_node<int> other_node{2, &node, nullptr, nullptr};
+    REQUIRE(other_node.parent == &node);
+    REQUIRE(other_node.left == nullptr);
+    REQUIRE(other_node.right == nullptr);
+
+    tree_node<std::vector<int>> vec_node{std::vector{1, 2, 3}, nullptr, nullptr, nullptr};
+    REQUIRE(vec_node.value[0] == 1);
+    REQUIRE(vec_node.value[1] == 2);
+    REQUIRE(vec_node.value[2] == 3);
+    REQUIRE(vec_node.parent == nullptr);
+    REQUIRE(vec_node.left == nullptr);
+    REQUIRE(vec_node.right == nullptr);
 }
