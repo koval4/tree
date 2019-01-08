09cd00536eaf7e22cb0825b1555784249c6485eb
Roman Kovalivskiy <koval304@gmail.com>
2019-01-08 02:44:10 +0200

    Add tree class
    tree class is a data structure user supposed to interact with. It
    should be default-constructible with empty state. It should propagate
    noexceptness, copy and move constructors. It should support
    user-defined allocator.

--- include/tree.h
+++ include/tree.h
@@ -3,6 +3,8 @@
 
 #include <type_traits>
 #include <utility>
+#include <memory>
+#include <cassert>
 
 namespace detail {
     template <typename T, bool = std::is_copy_constructible_v<T>, bool = std::is_move_constructible_v<T>>
@@ -97,7 +99,7 @@ struct tree_node_impl {
 };
 
 template <typename T>
-struct tree_node : tree_node_impl<T>, private detail::enable_special_members<T> {
+struct tree_node : private tree_node_impl<T>, private detail::enable_special_members<T> {
     template <typename U = T,
               std::enable_if_t<
                   !std::is_same_v<tree_node<T>, std::decay_t<U>> &&
@@ -123,6 +125,129 @@ struct tree_node : tree_node_impl<T>, private detail::enable_special_members<T> 
               tree_node<T>* left,
               tree_node<T>* right) noexcept(std::is_nothrow_constructible_v<T, U&&>)
         : tree_node_impl<T>{std::forward<U>(value), parent, left, right} {}
+
+    tree_node* left() const {
+        return reinterpret_cast<tree_node*>(tree_node_impl<T>::left);
+    }
+
+    tree_node* right() const {
+        return reinterpret_cast<tree_node*>(tree_node_impl<T>::right);
+    }
+
+    tree_node* parent() const {
+        return reinterpret_cast<tree_node*>(tree_node_impl<T>::parent);
+    }
+
+    T& value() {
+        return tree_node_impl<T>::value;
+    }
+
+    const T& value() const {
+        return tree_node_impl<T>::value;
+    }
+};
+
+template <typename T, typename Allocator = std::allocator<tree_node<T>>>
+struct tree_storage {
+    using allocator_traits = std::allocator_traits<Allocator>;
+
+    tree_storage(Allocator alloc = Allocator{}) noexcept
+        : alloc{std::move(alloc)}
+        , root{nullptr} {}
+
+    template <typename U,
+              std::enable_if_t<
+                  !std::is_same_v<tree_storage<T>, std::decay_t<U>> &&
+                  !std::is_convertible_v<U, T> &&
+                  std::is_constructible_v<tree_node<T>, U&&>, int> = 0>
+    explicit tree_storage(U&& value, Allocator alloc = Allocator{}) noexcept(std::is_nothrow_constructible_v<tree_node<T>, U&&>)
+        : alloc{std::move(alloc)}
+        , root{create_node(this->alloc, std::forward<U>(value))} {}
+
+    template <typename U,
+              std::enable_if_t<
+                  !std::is_same_v<tree_storage<T>, std::decay_t<U>> &&
+                  std::is_convertible_v<U, T> &&
+                  std::is_constructible_v<tree_node<T>, U&&>, int> = 0>
+    tree_storage(U&& value, Allocator alloc = Allocator{}) noexcept(std::is_nothrow_constructible_v<tree_node<T>, U&&>)
+        : alloc{std::move(alloc)}
+        , root{create_node(this->alloc, std::forward<U>(value))} {}
+
+    tree_storage(const tree_storage& other) noexcept(std::is_nothrow_copy_constructible_v<tree_node<T>>)
+        : alloc{other.alloc}
+        , root{create_node(this->alloc, other.root)} {}
+
+    tree_storage(tree_storage&& other) noexcept(std::is_nothrow_move_constructible_v<tree_node<T>>)
+        : alloc{std::move(other.alloc)}
+        , root{other.root} {}
+
+    virtual ~tree_storage() noexcept {
+        clear();
+    }
+
+    void clear_node_impl(tree_node<T>* node) noexcept {
+        assert(node != nullptr);
+        if (node->left() != nullptr) {
+            clear_node_impl(node->left());
+        }
+        if (node->right() != nullptr) {
+            clear_node_impl(node->right());
+        }
+
+        allocator_traits::destroy(alloc, node);
+        allocator_traits::deallocate(alloc, node, 1);
+    }
+
+    template <typename... Args>
+    static tree_node<T>* create_node(Allocator& alloc, Args&&... args) noexcept(std::is_nothrow_constructible_v<T, Args&&...>) {
+        tree_node<T>* node = allocator_traits::allocate(alloc, 1);
+        allocator_traits::construct(alloc, std::forward<Args>(args)...);
+        return node;
+    }
+
+    void clear() noexcept {
+        if (root != nullptr) {
+            clear_node_impl(root);
+        }
+    }
+
+    Allocator alloc;
+    tree_node<T>* root;
+};
+
+template <typename T, typename Allocator = std::allocator<tree_node<T>>>
+class tree
+    : private tree_storage<T, Allocator>
+    , private detail::enable_special_members<T> {
+    using base = tree_storage<T, Allocator>;
+
+public:
+    using allocator_type  = Allocator;
+    using value_type      = T;
+    using node_type       = tree_node<T>;
+    using reference       = value_type&;
+    using const_reference = const value_type&;
+    using pointer         = value_type*;
+    using const_pointer   = const value_type*;
+    using size_type       = size_t;
+    using difference_type = ptrdiff_t;
+
+    tree() = default;
+
+    explicit tree(Allocator alloc) noexcept
+        : base{std::move(alloc)} {};
+
+    size_type size() const noexcept {
+        return 0;
+    }
+
+    bool empty() const noexcept {
+        return base::root == nullptr;
+    }
+
+    void clear() noexcept {
+        base::clear();
+    }
 };
 
 #endif // TREE_H_INCLUDED
--- test/test.cpp
+++ test/test.cpp
@@ -46,27 +46,52 @@ TEST_CASE("Tree node constructed", "[tree_node]") {
     //    "tree_node should propagate move constructor presence");
 
     tree_node<int> node{1};
-    REQUIRE(node.value == 1);
-    REQUIRE(node.parent == nullptr);
-    REQUIRE(node.left == nullptr);
-    REQUIRE(node.right == nullptr);
+    REQUIRE(node.value() == 1);
+    REQUIRE(node.parent() == nullptr);
+    REQUIRE(node.left() == nullptr);
+    REQUIRE(node.right() == nullptr);
 
     tree_node<int> another_node{node};
-    REQUIRE(another_node.value == 1);
-    REQUIRE(another_node.parent == nullptr);
-    REQUIRE(another_node.left == nullptr);
-    REQUIRE(another_node.right == nullptr);
+    REQUIRE(another_node.value() == 1);
+    REQUIRE(another_node.parent() == nullptr);
+    REQUIRE(another_node.left() == nullptr);
+    REQUIRE(another_node.right() == nullptr);
 
     tree_node<int> other_node{2, &node, nullptr, nullptr};
-    REQUIRE(other_node.parent == &node);
-    REQUIRE(other_node.left == nullptr);
-    REQUIRE(other_node.right == nullptr);
+    REQUIRE(other_node.parent() == &node);
+    REQUIRE(other_node.left() == nullptr);
+    REQUIRE(other_node.right() == nullptr);
 
     tree_node<std::vector<int>> vec_node{std::vector{1, 2, 3}, nullptr, nullptr, nullptr};
-    REQUIRE(vec_node.value[0] == 1);
-    REQUIRE(vec_node.value[1] == 2);
-    REQUIRE(vec_node.value[2] == 3);
-    REQUIRE(vec_node.parent == nullptr);
-    REQUIRE(vec_node.left == nullptr);
-    REQUIRE(vec_node.right == nullptr);
+    REQUIRE(vec_node.value()[0] == 1);
+    REQUIRE(vec_node.value()[1] == 2);
+    REQUIRE(vec_node.value()[2] == 3);
+    REQUIRE(vec_node.parent() == nullptr);
+    REQUIRE(vec_node.left() == nullptr);
+    REQUIRE(vec_node.right() == nullptr);
+}
+
+TEST_CASE("Tree constructed", "[tree]") {
+    // compile-time checks
+    static_assert(
+        std::is_copy_constructible_v<tree<int>>,
+        "tree should propagate copy constructor presence");
+    static_assert(
+        std::is_move_constructible_v<tree<int>>,
+        "tree should propagate move constructor presence");
+
+    static_assert(
+        !std::is_copy_constructible_v<tree<no_copy>>,
+        "tree should propagate copy constructor presence");
+    static_assert(
+        std::is_move_constructible_v<tree<no_copy>>,
+        "tree should propagate move constructor presence");
+
+    static_assert(
+        std::is_copy_constructible_v<tree<no_move>>,
+        "tree should propagate copy constructor presence");
+
+    tree<int> _1;
+    REQUIRE(_1.empty());
+    REQUIRE(_1.size() == 0);
 }
